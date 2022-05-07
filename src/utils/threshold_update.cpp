db0e83f696681300311c7713641ac26923647a63
Dong Kim <dkim1232@ucompute1.physics.umd.edu>
2022-05-07 15:08:56 -0400

    redesigned the threshold udpate

--- src/utils/threshold_update.cpp
+++ src/utils/threshold_update.cpp
@@ -5,7 +5,7 @@ thrshold_update::threshold_update(float threshold, int index, vector<vector<vect
     this->threshold = threshold;
 }
     
-float thrshold_update::update(vector<float> weight){
+int thrshold_update::update(vector<int> feature, vector<int> sample){
     
     vector<int> a;
     float e, e2;
@@ -17,106 +17,34 @@ float thrshold_update::update(vector<float> weight){
     // increase the value with 10 size
     while()
     {
-        WeakClassifier Weak.WeakClassifier(feature, width, height, startRow, startCol, polarity, threshold);
-        for(int i=0; i<index; i++)
+		int T =0, S=0, t=0, s=0;
+		int e,e2;
+		vector<int> threshold = feature
+		sort(threshold.begin(), threshold.end());
+		int i;
+        for(i=0; i<threshold.size(); i++)
         {
-            if(polarity[i] == 1)
-                T += weight[i];
-            else
-                S += weight[i];
-            if(Weak.classifyImage(img[i]) == 1 && polarity[i] == 1)
-                t += weight[i];
-            else
-                s += weight[i];
+			for(int j=0; j<feature.size(); j++)
+			{
+				if(sampe[j] >1)
+					T += feature[j];
+				if(feature[j]<threshold[i] && sample[j] > 1)
+					S += feature[j];
+				if(sampe[j] <1)
+					T += feature[j];
+				if(feature[j]<threshold[i] && sample[j] < 1)
+					S += feature[j];
+			}
+			e = min(S+(t-s), s+(T-S));
+			if(i!=0 && e>e2)
+			{
+				i--;
+				break;
+			}
+			e=e2;
         }
-        e = min(S+t-s, s + T - S);
-        threshold += 10;
-        if(count != 0 && e>e2)
-        {
-            threshold -= 10;
-            break;
-        }
-        e2=e;
+		
     }
-    
-    // increase the value with 1 size
-    count = 0;
-    while()
-    {
-        WeakClassifier Weak.WeakClassifier(feature, width, height, startRow, startCol, polarity, threshold);
-        for(int i=0; i<index; i++)
-        {
-            if(polarity[i] == 1)
-                T += weight[i];
-            else
-                S += weight[i];
-            if(Weak.classifyImage(img[i]) == 1 && polarity[i] == 1)
-                t += weight[i];
-            else
-                s += weight[i];
-        }
-        e = min(S+t-s, s + T - S);
-        threshold += 1;
-        if(count != 0 && e>e2)
-        {
-            threshold -= 1;
-            break;
-        }
-        e2=e;
-    }
-    
-    //increase the threshold in .1 size
-    count =0;
-    while()
-    {
-        WeakClassifier Weak.WeakClassifier(feature, width, height, startRow, startCol, polarity, threshold);
-        for(int i=0; i<index; i++)
-        {
-            if(polarity[i] == 1)
-                T += weight[i];
-            else
-                S += weight[i];
-            if(Weak.classifyImage(img[i]) == 1 && polarity[i] == 1)
-                t += weight[i];
-            else
-                s += weight[i];
-        }
-        e = min(S+t-s, s + T - S);
-        threshold += .1;
-        if(count != 0 && e>e2)
-        {
-            threshold -= .1;
-            break;
-        }
-        e2=e;
-    }
-    
-    //increase the threshold in .01 size
-    count =0;
-    while()
-    {
-        WeakClassifier Weak.WeakClassifier(feature, width, height, startRow, startCol, polarity, threshold);
-        for(int i=0; i<index; i++)
-        {
-            if(polarity[i] == 1)
-                T += weight[i];
-            else
-                S += weight[i];
-            if(Weak.classifyImage(img[i]) == 1 && polarity[i] == 1)
-                t += weight[i];
-            else
-                s += weight[i];
-        }
-        e = min(S+t-s, s + T - S);
-        threshold += .01;
-        if(count != 0 && e>e2)
-        {
-            threshold -= .01;
-            break;
-        }
-        e2=e;
-    }
-    
-    return threshold; // final value
+    return threshold[i];
 }
 
